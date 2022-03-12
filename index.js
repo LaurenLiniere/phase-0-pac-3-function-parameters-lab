afd1434fc24a40e9cdfce3b6c4c4e7ff628d52a8
Lauren <laurenliniere@gmail.com>
2022-03-12 19:25:22 +1100

    Completed Assessment

---
+++ index.js
@@ -0,0 +1,81 @@
+/*
+function sayHelloTo(firstName = "User") {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo();
+*/
+
+
+
+function introductionWithLanguageOptional(firstName = "User", language = "Javascript") {
+    console.log(`Hello, ${firstName}! Today we are learning ${language}`);
+}
+
+introductionWithLanguageOptional();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const age = 12
+
+let isAdult, canWork, canEnlist, canDrink;
+
+if(age >=21) {
+    canWork = true;
+    canEnlist = true;
+    isAdult= true;
+    canDrink = true;
+}
+else if (age >= 18) {
+    canWork = true;
+    canEnlist = true;
+    isAdult= true;
+    canDrink = true; 
+}
+else if (age <=16) {
+    canWork = true;
+    canEnlist = false;
+    isAdult= false;
+    canDrink = false;
+}
+else {
+    console.log("No more options")
+}
+
+console.log("canWork" + canWork);
+console.log("canDrink" + canDrink);
+console.log("canEnlist" + canEnlist);
+console.log("isAdult" + isAdult);
+*/
+
